6fb30e73bf95d4924d3dffaa22afc4564ca18f43
Christopher <chris.calascione@gmail.com>
2015-08-10 23:00:16 -0700

    run simple tests, create method that passes all tests

--- src/ClockAngle.php
+++ src/ClockAngle.php
@@ -1,11 +1,17 @@
 <?php
-
+//will find the smallest angle between the hour and minute hands on an analog clock
     class ClockAngle
     {
         function angleBetweenClockHands($time)
         {
-            if ($time == "12") {
+            if ($time == 12) {
                 return 0 . " Degrees";
+            } elseif ($time >= 1 && $time <= 6) {
+                $degrees = $time * 30;
+                return $degrees . " Degrees";
+            } elseif ($time > 6 && $time < 12) {
+                $degrees = 360 - ($time * 30);
+                return $degrees . " Degrees";
             }
         }
     }
--- tests/ClockAngleTest.php
+++ tests/ClockAngleTest.php
@@ -7,11 +7,121 @@
         function test_clockAngle12()
         {
             $test_ClockAngleTest = new ClockAngle;
-            $input = "12";
+            $input = 12;
 
             $result = $test_ClockAngleTest->angleBetweenClockHands($input);
 
             $this->assertEquals("0 Degrees", $result);
+        }
+
+        function test_clockAngle1()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 1;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("30 Degrees", $result);
+        }
+
+        function test_clockAngle2()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 2;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("60 Degrees", $result);
+        }
+
+        function test_clockAngle3()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 3;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("90 Degrees", $result);
+        }
+
+        function test_clockAngle4()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 4;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("120 Degrees", $result);
+        }
+
+        function test_clockAngle5()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 5;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("150 Degrees", $result);
+        }
+
+        function test_clockAngle6()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 6;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("180 Degrees", $result);
+        }
+
+        function test_clockAngle7()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 7;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("150 Degrees", $result);
+        }
+
+        function test_clockAngle8()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 8;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("120 Degrees", $result);
+        }
+
+        function test_clockAngle9()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 9;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("90 Degrees", $result);
+        }
+
+        function test_clockAngle10()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 10;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("60 Degrees", $result);
+        }
+
+        function test_clockAngle11()
+        {
+            $test_ClockAngleTest = new ClockAngle;
+            $input = 11;
+
+            $result = $test_ClockAngleTest->angleBetweenClockHands($input);
+
+            $this->assertEquals("30 Degrees", $result);
         }
     }
 
